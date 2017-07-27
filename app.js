00154dfeae63dbd0add9b6e88b209230e713de13
suhas <suhas.m@bvbi-infotech.com>
2017-07-27 18:27:20 +0530

    Books api included

--- app.js
+++ app.js
@@ -18,23 +18,97 @@ app.set('view engine', 'jade');*/
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//APIS
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/bookshop');
+
+var Books = require('./models/books.js');
+//------>>>>>>>Post API <<<<<<-------//
+app.post('/books', function (req, res) {
+  var book = req.body;
+  Books.create(book, function (err, books) {
+    if (err) {
+      throw err;
+    } else {
+      res.json(books);
+    }
+  })
+});
+
+//------->>>>Get Book List <<<<<<-----
+app.get('/books', function (req, res) {
+  Books.find(function (err, books) {
+    if (err) {
+      throw err;
+    } else {
+      res.json(books);
+    }
+  })
+})
+//------->>>>Delete Book <<<<<<-----
+app.delete('/books/:_id', function (req, res) {
+  var query = {
+    _id: req.params._id
+  };
+  Books.remove(query, function (err, books) {
+    if (err) {
+      throw err;
+    } else {
+      res.json(books);
+    }
+  })
+});
+//------->>>>Update Book <<<<<<-----
+app.put('/books/:_id', function (req, res) {
+  var book = req.body;
+  var query = {
+    _id: req.params._id
+  };
+
+//If field doesnt exists $set will add the field
+  var update = {
+    '$set': {
+      title: book.title,
+      description: book.description,
+      price: book.price,
+      image : book.image
+    }
+  }
+
+  //When true returns the updated document
+  var options = {
+    new: true
+  };
+
+  Books.findOneAndUpdate(query, update, options, function (err, books) {
+    if (err) {
+      throw err;
+    } else {
+      res.json(books);
+    }
+  })
+});
+//ENDAPIS
+
 app.get('*', function (req, res) {
-    res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
 })
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
